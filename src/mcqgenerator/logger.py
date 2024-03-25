8bc45ff02cee0e84d8479cf13011efa49e5008a4
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-25 21:57:31 +0600

    logger added

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -1,14 +1,16 @@
-import logging
 import os
-from datetime import datetime
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
 
 
-LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
-log_path = os.path.join(os.getcwd(), "logs")
-os.makedirs(log_path,exist_ok=True)
-LOG_FILEPATH= os.path.join(log_path, LOG_FILE)
 logging.basicConfig(
     level=logging.INFO,
-    filename=LOG_FILEPATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+    format=logging_str,
+    handlers=[logging.FileHandler(log_filepath), logging.StreamHandler(sys.stdout)],
 )
