eaf847a2b3b1c8f1f51cad5925de8ce3cf1c1000
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-25 15:15:50 +0600

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='1.0.0',
+    author='aljaziz',
+    author_email='turjatheharbingerofdeath@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
